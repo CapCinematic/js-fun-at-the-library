dfd29863506282b9dc5832ea6ba11f865babc6ca
Isaiah Richardson <capcinematic@gmail.com>
2022-12-04 20:38:11 -0700

    shelfjs

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,38 @@
+function shelfBook(bookName, shelfName) {
+  if (shelfName.length < 3) {
+    shelfName.unshift(bookName);
+  }
+}
+
+function unshelfBook(bookName, shelfName) {
+  for (i = 0; i < shelfName.length; i++) {
+    if (bookName === shelfName[i].title) {
+      shelfName.splice(i, 1);
+    }
+  }
+}
+
+function listTitles(shelfName) {
+  var titles = [];
+  for (i = 0; i < shelfName.length; i++) {
+    titles.push(shelfName[i].title);
+  }
+  return titles.join(", ");
+};
+function searchShelf(shelfName, bookName){
+  for (i = 0; i < shelfName.length; i++){
+    if (bookName === shelfName[i].title){
+      return true
+    }
+
+  };
+  return false
+}
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
